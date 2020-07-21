3a5f66135e5fd1f37d689564e73bcdf39c46bd23
Shin <fyuneru0830@hotmail.com>
2020-07-22 01:29:33 +0900

    修改名字，新增数组

--- program.rb
+++ program.rb
@@ -1,8 +1,9 @@
 #!/usr/bin/ruby
 
-list = Array["三川一二三","田中平","山田花","井上光","中岩京介"]
+list = Array[ "三川一二三","田中平","山田花","井上光","中岩京介" ]
+listScore = Array[ 12,14,15,13,24 ]
 puts "#{list}"
- 
+puts "#{listScore}"
 aFile = File.new("input.txt", "r+")
 if aFile
    aFile.syswrite("ABCDEF")
